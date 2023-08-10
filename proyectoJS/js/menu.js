7b156390a05488c8bc9b78768598b897fcd161cf
Ariza01 <134986050+Ariza01@users.noreply.github.com>
2023-08-09 22:07:34 -0600

    Cambios

--- proyectoJS/js/menu.js
+++ proyectoJS/js/menu.js
@@ -1,15 +1,15 @@
-menuprincipal.innerHTML += `
+ menuprincipal.innerHTML += `
 <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="index.html">Inicio</a>
     <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
         aria-expanded="false" aria-label="Toggle navigation"></button>
     <div class="collapse navbar-collapse" id="collapsibleNavId">
         <ul class="navbar-nav me-auto mt-2 mt-lg-0">
             <li class="nav-item">
-                <a class="nav-link active" href="#" aria-current="page">Home <span class="visually-hidden">(current)</span></a>
+                <a class="nav-link active" href="listarcurso.html" aria-current="page">Listar Curso<span class="visually-hidden">(current)</span></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
+                <a class="nav-link" href="https://paginas-web-cr.com/ApiPHP/">Api</a>
             </li>
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
