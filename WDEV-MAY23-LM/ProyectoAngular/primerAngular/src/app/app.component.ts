5a61a0d67c0a7b134e69cc889a09a90552fb1906
Ariza01 <134986050+Ariza01@users.noreply.github.com>
2023-11-12 22:08:34 -0600

    v

--- WDEV-MAY23-LM/ProyectoAngular/primerAngular/src/app/app.component.ts
+++ WDEV-MAY23-LM/ProyectoAngular/primerAngular/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Cursos } from './models/cursos';
+import { Respuestacursos } from './models/respuestacursos';
 // import importar librerias o clases
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
@@ -12,40 +13,71 @@ export class AppComponent implements OnInit{
   //Atributos
   title = 'primerAngular';
 
-  arregloDatos: Cursos[] = [
-    { id: "1", nombre : "html5", descripcion : "111", tiempo : "1", usuario : "Santiago"},
-    { id: "2", nombre : "css3", descripcion : "222", tiempo : "22", usuario : "Santiago"},
-    { id: "3", nombre : "js", descripcion : "333", tiempo : "333", usuario : "Santiago"},
-    { id: "4", nombre : "jquery", descripcion : "444", tiempo : "444", usuario : "Santiago"},
-    { id: "5", nombre : "bootstrap", descripcion : "555", tiempo : "5555", usuario : "Santiago"}
-  ]
+  // arregloDatos: Cursos[] = [
+  //   { id: "1", nombre : "html5", descripcion : "111", tiempo : "1", usuario : "Santiago"},
+  //   { id: "2", nombre : "css3", descripcion : "222", tiempo : "22", usuario : "Santiago"},
+  //   { id: "3", nombre : "js", descripcion : "333", tiempo : "333", usuario : "Santiago"},
+  //   { id: "4", nombre : "jquery", descripcion : "444", tiempo : "444", usuario : "Santiago"},
+  //   { id: "5", nombre : "bootstrap", descripcion : "555", tiempo : "5555", usuario : "Santiago"}
+  // ]
   
-  //arregloDatos: Cursos[] = [];
+
+  arregloDatos: Cursos[] = [];
 
 
-  //constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {}
 
 
-  ngOnInit(): void {
-    //this.obtenerDatos();
-  }
-
-
-  // obtenerDatos(){
-  //   const url = "https://paginas-web-cr.com/ApiPHP/apis/ListaCurso.php"
-
-
-  //   this.http.get<Cursos[]>(url).subscribe(
-  //     (response) => {
-  //       //this.arregloDatos = 
-  //       console.log(response);
-  //     },
-  //     (error) =>{
-  //       console.error('Error en la carga de datos', error);
-  //     }
-  //   )
-
+  // ngOnInit(): void {
+  //   //this.obtenerDatos();
   // }
+
+
+  obtenerDatos(){
+    const url = "https://paginas-web-cr.com/ApiPHP/apis/ListaCurso.php"
+
+
+    this.http.get<Respuestacursos>(url).subscribe(
+      (response) => {
+        this.arregloDatos = response.data;
+        console.log(response.data);
+      },
+      (error) =>{
+        console.error('Error en la carga de datos', error);
+      }
+    )
+
+  }
+  ngOnInit() {
+    this.obtenerDatos();
+  }
+  enviarSolicitudPost() {
+    const url = 'https://paginas-web-cr.com/ApiPHP/apis/ListaCurso.php';
+
+    // Datos que deseas enviar en la solicitud POST
+    const data = {
+      parametro1: 'valor1',
+      parametro2: 'valor2'
+      // Agrega más datos segun tus necesidades 
+    };
+
+    //Configura las cabeceras para la solicitud POST
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json' // Ajusta el tipo de contenido segun el requerimiento
+    })
+
+    // Realiza la solicitud POST
+    this.http.post(url, data, {headers}).subscribe(
+      (response) => {
+        // Maneja la respuesta de la API
+        console.log('respuesta de la API:', response);
+      },
+      (error) => {
+        // Maneja los errores de la solicitud
+        console.error('Errer:', error);
+      }
+    );
+  }
 
 }
 
--- WDEV-MAY23-LM/ProyectoAngular/primerAngular/src/app/app.module.ts
+++ WDEV-MAY23-LM/ProyectoAngular/primerAngular/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+//Se necesita el client http
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +13,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
